925c5535d32866c6d4f7fcf1c06c2f4f0d15c4f8
Enoquef <nettolinhares18@gmail.com>
2020-03-18 15:59:21 -0300

    implementando command

--- src/main/java/br/ufpb/dcx/aps/escalonador/Command/ADDPTFcommand.java
+++ src/main/java/br/ufpb/dcx/aps/escalonador/Command/ADDPTFcommand.java
@@ -1,5 +1,36 @@
 package br.ufpb.dcx.aps.escalonador.Command;
 
-public class ADDPTFcommand {
+import br.ufpb.dcx.aps.escalonador.Escalonador;
+
+public class ADDPTFCommand implements Command {
+
+	private Escalonador escalonador;
+	private String processo;
+	private int duracao;
+
+	public ADDPTFCommand(String nomeProcesso) {
+		this.processo = nomeProcesso;
+	}
+
+	public ADDPTFCommand(String nomeProcesso, int duracaoProcesso) {
+		this.processo = nomeProcesso;
+		this.duracao = duracaoProcesso;
+	}
+
+	@Override
+	public String executar() {
+		getEscalonador().adicionarProcessoTempoFixo(processo, duracao);
+		return null;
+	}
+
+	@Override
+	public Escalonador getEscalonador() {
+		return this.escalonador;
+	}
+
+	@Override
+	public void setEscalonador(Escalonador escalonador) {
+		this.escalonador = escalonador;
+	}
 
 }
