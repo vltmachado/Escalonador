51e42e150973f29d9bf1bad7bb2146601e8383c1
Igor Felipe <igor.felipe@ce.ufpb.br>
2020-03-18 18:24:01 -0300

    terminado bloquear processo e retormar processo do command

--- src/main/java/br/ufpb/dcx/aps/escalonador/Command/RetomarPCommand.java
+++ src/main/java/br/ufpb/dcx/aps/escalonador/Command/RetomarPCommand.java
@@ -1,5 +1,30 @@
 package br.ufpb.dcx.aps.escalonador.Command;
 
-public class RetomarPCommand {
+import br.ufpb.dcx.aps.escalonador.Escalonador;
+
+public class RetomarPCommand implements Command {
+	private Escalonador escalonador;
+	private String processo;
+	
+	public RetomarPCommand(String processo) {
+		this.processo = processo;
+	}
+
+	@Override
+	public String executar() {
+		getEscalonador().retomarProcesso(processo);
+		return null;
+	}
+
+	@Override
+	public Escalonador getEscalonador() {
+		return this.escalonador;
+	}
+
+	@Override
+	public void setEscalonador(Escalonador escalonador) {
+		this.escalonador = escalonador;
+	}
+
 
 }
