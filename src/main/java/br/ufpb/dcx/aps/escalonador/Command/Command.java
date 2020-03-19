094a725902bf76a495a16c00779121e9cc0342ae
vltmachado <valter.machado@dce.ufpb.br>
2020-03-19 04:14:14 -0300

    Finalizando

--- src/main/java/br/ufpb/dcx/aps/escalonador/Command/Command.java
+++ src/main/java/br/ufpb/dcx/aps/escalonador/Command/Command.java
@@ -1,5 +1,29 @@
 package br.ufpb.dcx.aps.escalonador.Command;
 
-public class Command {
+import br.ufpb.dcx.aps.escalonador.Escalonador;
 
+public class FinalizarPCommand implements Command{
+	
+	private Escalonador escalonador;
+	private String processo;
+	
+	public FinalizarPCommand(String processo) {
+		this.processo = processo;
+	}
+
+	@Override
+	public String executar() {
+		getEscalonador().finalizarProcesso(processo);
+		return null;
+	}
+
+	@Override
+	public Escalonador getEscalonador() {
+		return this.escalonador;
+	}
+
+	@Override
+	public void setEscalonador(Escalonador escalonador) {
+		this.escalonador = escalonador;
+	}
 }
