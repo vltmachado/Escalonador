ff5ec1f81137334c5d4ec40fb68ff7599d7559a6
vltmachado <valter.machado@dce.ufpb.br>
2020-03-19 15:20:07 -0300

    Corrigindo FinalizaeCommand

--- src/main/java/br/ufpb/dcx/aps/escalonador/Command/FinalizarPCommand.java
+++ src/main/java/br/ufpb/dcx/aps/escalonador/Command/FinalizarPCommand.java
@@ -3,11 +3,11 @@ package br.ufpb.dcx.aps.escalonador.Command;
 import br.ufpb.dcx.aps.escalonador.Escalonador;
 
 public class FinalizarPCommand implements Command{
-	/*
+	
 	private Escalonador escalonador;
 	private String processo;
 	
-	/*
+	
 	public FinalizarPCommand(String processo) {
 		this.processo = processo;
 	}
